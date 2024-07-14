eae1ea30d357793322be55249d1d6bc6bb6a712c
WalterW4 <167467899+WalterW4@users.noreply.github.com>
2024-07-15 00:00:00 +0500

    update js script

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ function startGame() {
     document.getElementById('guess').value = '';
     document.getElementById('guess').disabled = false;
 }
+
+function checkGuess() {
+    const userGuess = Number(document.getElementById('guess').value);
+    attempts++;
+
+    if (userGuess < 1 || userGuess > 100) {
+        document.getElementById('result').innerText = 'Please enter a number between 1 and 100.';
+    } else if (userGuess < randomNumber) {
+        document.getElementById('result').innerText = 'Too low! Try again.';
+    } else if (userGuess > randomNumber) {
+        document.getElementById('result').innerText = 'Too high! Try again.';
+    } else {
+        document.getElementById('result').innerText = `Congratulations! You guessed the number ${randomNumber} in ${attempts} attempts.`;
+        document.getElementById('guess').disabled = true;
+    }
+}
+
 // Start the game when the page loads
 window.onload = startGame;
